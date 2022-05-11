3859907fbfd9114d9e49169cb2f000b8161b57d4
KanZ <105295347+KaaanZ@users.noreply.github.com>
2022-05-11 11:51:24 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,35 @@
-let name;
-console.log.name
+function dodawanie()
+{
+
+    let value1 = document.getElementById('input1').value
+    let value2 = document.getElementById('input2').value
+    let suma = (+value1) + (+value2)
+    let wynikoutput = document.getElementById('wynik').textContent = (suma)
+}
+
+function odejmowanie()
+{
+    
+    let value1 = document.getElementById('input1').value
+    let value2 = document.getElementById('input2').value
+    let sumaodejmowania = (+value1) - (+value2)
+    let wynikoutput = document.getElementById('wynik').textContent = (sumaodejmowania)
+
+}
+
+function mnozenie()
+{
+
+    let value1 = document.getElementById('input1').value
+    let value2 = document.getElementById('input2').value
+    let sumamnozenia = (+value2) * (+value1)
+    let wynikoutput = document.getElementById('wynik').textContent = (sumamnozenia)
+}
+
+function dzielenie()
+{
+    let value1 = document.getElementById('input1').value
+    let value2 = document.getElementById('input2').value
+    let sumamdzielenia = (+value1) / (+value2)
+    let wynikoutput = document.getElementById('wynik').textContent = (sumamdzielenia)
+}
